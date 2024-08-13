14d6e8109bdb6a104a516c01b28eb1b2840276b0
AsBorn007 <pk175040@gmail.com>
2024-08-13 14:53:56 +0530

    Map Array Commit

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-let show = document.querySelector(".hero-section");
-let img = document.createElement("img");
+// let show = document.querySelector(".hero-section");
+// let img = document.createElement("img");
 
-img.src = '/img/b4.jpg';
-img.style.width = "100%";
+// img.src = '/img/b4.jpg';
+// img.style.width = "100%";
 
-show.appendChild(img);
+// show.appendChild(img);
 
 
-let min = document.querySelector(".minus");
-let dis = document.querySelector("#display");
-let plus = document.querySelector(".plus");
+// let min = document.querySelector(".minus");
+// let dis = document.querySelector("#display");
+// let plus = document.querySelector(".plus");
 
-let num = 0;
+// let num = 0;
 
-min.addEventListener("click", () => {
-    num --;
-    if(num < 0) {
-        dis.value = 0;
-    }else {
-        dis.value = num;
-    }
-    console.log(min, "minus");
-})
+// min.addEventListener("click", () => {
+//     num --;
+//     if(num < 0) {
+//         dis.value = 0;
+//     }else {
+//         dis.value = num;
+//     }
+//     console.log(min, "minus");
+// })
 
-plus.addEventListener("click", () => {
-    num ++;
-    if(num > 0) {
-        dis.value = num;
-    }
-    console.log(plus, "add");
-})
+// plus.addEventListener("click", () => {
+//     num ++;
+//     if(num > 0) {
+//         dis.value = num;
+//     }
+//     console.log(plus, "add");
+// })
 
 let arr = [1,2,3,4,5]
-let b = arr.reduce(accum, curtval)
+let a = arr.map((value)=>{
+    return value * 3
+})
+console.log(a,"this is new arry jo map return krke deta hai ")
+console.log(arr, "ye vo array hai original ko he console me print krke dega")
 
 
         
